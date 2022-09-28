246c29a1596489f2aa38825451a0aa185f19978e
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-09-29 01:58:35 +0600

    adding map function

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,77 +1,24 @@
-import React from "react";
-import "./Cards.css";
+import React, { useEffect, useState } from "react";
+import Card from "../Card/Card";
+
 const Cards = () => {
+  const [fitnesses, setFitnesses] = useState([]);
+  useEffect(() => {
+    fetch("fitness.json")
+      .then((res) => res.json())
+      .then((data) => setFitnesses(data));
+  }, []);
+  
   return (
     <div className="container mt-5">
-        <h4 className="mb-5">Select todays excersize</h4>
-      <div className="cards-container">        
+      <h4 className="mb-5">Select todays excersize</h4>
+      <div className="cards-container">
         <div className="row g-4">
           <div className="col-lg-8">
             <div className="row g-4">
-              <div className="col-lg-4">
-                <div className="card">
-                  <img src="" alt="" />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="card">
-                  <img src="" alt="" />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="card">
-                  <img src="" alt="" />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-2 activity-container">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="card">
-                  <img src="" alt="" />
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" className="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
+              {fitnesses.map((fitness) => (                
+                  <Card key={fitness.id} fitness={fitness}></Card>                
+              ))}
             </div>
           </div>
         </div>
@@ -79,5 +26,6 @@ const Cards = () => {
     </div>
   );
 };
+
 
 export default Cards;
