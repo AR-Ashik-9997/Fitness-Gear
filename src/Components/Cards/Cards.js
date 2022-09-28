067739e587f43a494faa2592b72e35e953ecbb63
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-09-28 23:16:54 +0600

    design to cards group and activity group

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,11 +1,83 @@
-import React from 'react';
-
+import React from "react";
+import "./Cards.css";
 const Cards = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="container mt-5">
+        <h4 className="mb-5">Select todays excersize</h4>
+      <div className="cards-container">        
+        <div className="row g-4">
+          <div className="col-lg-8">
+            <div className="row g-4">
+              <div className="col-lg-4">
+                <div className="card">
+                  <img src="" alt="" />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="card">
+                  <img src="" alt="" />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="card">
+                  <img src="" alt="" />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-2 activity-container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="card">
+                  <img src="" alt="" />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
